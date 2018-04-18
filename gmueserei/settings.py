df177464d1a875a0eefed2fc55f65573f0f6c11e
Gmueserei <webmaster@gmueserei.ch>
2018-04-18 21:15:32 +0200

    ssl required

--- gmueserei/settings.py
+++ gmueserei/settings.py
@@ -104,6 +104,7 @@ AUTHENTICATION_BACKENDS = (
 
 MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
