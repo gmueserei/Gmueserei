531707554902c38b6a16bb37ca9ae359295ae121
Gmueserei <webmaster@gmueserei.ch>
2018-05-23 12:02:33 +0200

    changes

--- gmueserei/settings.py
+++ gmueserei/settings.py
@@ -199,7 +199,7 @@ IMAGES = {'status_100': '/static/img/status_100.png',
             'single_full': '/static/img/single_full.png',
             'single_empty': '/static/img/single_empty.png',
             'single_core': '/static/img/single_core.png',
-            'core': '/static/gm/img/core.png'
+            'core': '/static/img/core.png'
 }
 EMAILS = {
     'welcome': 'mails/welcome_mail.txt',
