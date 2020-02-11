8e4268ee51b5490fe6ab4c6777385ee0ee38657e
root <root@31991.hostserv.eu>
2020-02-11 20:09:27 +0100

    wsgi

--- gmueserei/wsgi.py
+++ gmueserei/wsgi.py
@@ -4,7 +4,6 @@ WSGI config for gmueserei project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -12,13 +11,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gmueserei.settings")
 
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
