88d3f0ed6486f2ee42f2678ffbffcd661f22ab8c
drops5i <149464900+drops5i@users.noreply.github.com>
2024-04-01 21:27:28 +0200

    Update settings.py

--- gmueserei/settings.py
+++ gmueserei/settings.py
@@ -173,9 +173,7 @@ INFO_EMAIL = "mini@gmueserei.ch"
 SERVER_URL = "gmueserei.ch"
 BUSINESS_REGULATIONS = ""
 BYLAWS = "https://www.gmueserei.ch/index.php?page=dokumente"
-STYLES = {
-    'template' : ['gm/css/gmueserei.css'],
-    'static': ['gm/css/personal.css']}
+STYLES = {'static': ['gm/css/personal.css']}
 FAVICON = {'static': ['img/favicono.ico']}
 FAQ_DOC = ""
 EXTRA_SUB_INFO = ""
