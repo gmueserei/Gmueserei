a9363c1dd057a774ee25db745516636ac52918fd
drops5i <149464900+drops5i@users.noreply.github.com>
2025-07-17 19:41:49 +0200

    phone added

--- gmueserei/settings.py
+++ gmueserei/settings.py
@@ -168,7 +168,8 @@ ORGANISATION_ADDRESS = {"name":"Genossenschaft GMÜESEREI SISSACH",
             "number" : "25f",
             "zip" : "4450",
             "city" : "Sissach",
-            "extra" : "T 078 480 35 99"}
+            "phone" : "T 078 480 35 99",
+            "extra" : ""}
 ORGANISATION_BANK_CONNECTION = {"PC" : " ",
             "IBAN" : "CH1408390035190010009",
             "BIC" : "ABSOCH22",
